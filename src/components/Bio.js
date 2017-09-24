ffb1c8d183711bd275a274a4a2201a180adae04f
Heeje Jeon <heeje@Heejes-MacBook-Pro.local>
2017-09-24 17:43:36 +0100

    First commit

---
+++ src/components/Bio.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <p
+        style={{
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Ptolemy Barnes`}
+          style={{
+            float: 'left',
+            marginRight: rhythm(1 / 4),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        Written by <strong>Ptolemy Barnes</strong>.
+      </p>
+    )
+  }
+}
+
+export default Bio
